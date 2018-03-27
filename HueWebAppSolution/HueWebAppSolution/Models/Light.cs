6445204abbf172f001b4b22024fb77a3eb8cec4f
Unknown <rattlingbean@gmail.com>
2018-03-27 23:14:02 +0100

    Added light/state models

---
+++ HueWebAppSolution/HueWebAppSolution/Models/Light.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HueWebAppSolution.Models
+{
+    public class Light
+    {
+        [JsonProperty(PropertyName = "state")]
+        public State State { get; set; }
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "modelid")]
+        public string ModelId { get; set; }
+        [JsonProperty(PropertyName = "swversion")]
+        public string SwVersion { get; set; }
+    }
+}
---
+++ HueWebAppSolution/HueWebAppSolution/Models/State.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HueWebAppSolution.Models
+{
+    public class State
+    {
+        [JsonProperty(PropertyName = "on")]
+        public bool On { get; set; }
+        [JsonProperty(PropertyName = "bri")]
+        public int Bri { get; set; }
+        [JsonProperty(PropertyName = "hue")]
+        public int Hue { get; set; }
+        [JsonProperty(PropertyName = "sat")]
+        public int Sat { get; set; }
+        [JsonProperty(PropertyName = "xy")]
+        public decimal[] Xy { get; set; }
+        [JsonProperty(PropertyName = "ct")]
+        public int Ct { get; set; }
+        [JsonProperty(PropertyName = "alert")]
+        public string Alert { get; set; }
+        [JsonProperty(PropertyName = "effect")]
+        public string Effect { get; set; }
+        [JsonProperty(PropertyName = "colormode")]
+        public string ColorMode { get; set; }
+        [JsonProperty(PropertyName = "reachable")]
+        public bool Reachable { get; set; }
+    }
+}
